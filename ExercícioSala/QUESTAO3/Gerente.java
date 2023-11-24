ebcf6b597dfa68872562c80614427bd2761ee164
Livia <liviaeusla@gmail.com>
2023-11-23 20:36:38 -0500

    Add calcularBonificação() no Funcionário gerente

--- ExercícioSala/QUESTAO3/Gerente.java
+++ ExercícioSala/QUESTAO3/Gerente.java
@@ -8,15 +8,19 @@ public class Gerente extends Funcionario {
         this.departamento = departamento;
         this.nivel = nivel;
     }
+    public double calcularBonificacao(){
+        return super.GetSalario() * 0.05;
+    }
+
     @Override
     public double CalcularSalarioFinal(){
-        double salarioFinal = (super.GetSalario() * this.nivel );
+        double salarioFinal = (super.GetSalario() * this.nivel ) + calcularBonificacao();
         return salarioFinal;
     }
 
     @Override
     public String toString() {
-        return super.toString() + "\nDepartamento: " + this.departamento + "\nNível: " + this.nivel;
+        return super.toString() + "\nBonificaçao de 5% (Incide no salario base): " + calcularBonificacao() + "\nDepartamento: " + this.departamento + "\nNível: " + this.nivel;
     }
 
 }
